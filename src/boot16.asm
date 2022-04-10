59c32d6683b9fad0abd13df4da0d920c82e533d9
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-10 18:45:35 -0400

    Update boot16.asm

--- src/boot16.asm
+++ src/boot16.asm
@@ -24,3 +24,31 @@ jmp $
 
 times 510-($-$$) db 0
 dw 0AA55h ; some BIOSes require this signature
+
+shutdown:
+        ; APM shutdown
+        mov ax, 0x5300
+        xor bx, bx
+        int 0x15
+        jc shutdown_fail
+
+        mov ax, 0x5303
+        xor bx, bx
+        int 0x15
+        jc shutdown_fail
+
+        mov ax, 0x5308
+        mov bx, 0x0001
+        mov cx, 0x0001
+        int 0x15
+        jc shutdown_fail
+
+        mov ax, 0x5307
+        mov bx, 0x0001
+        mov cx, 0x0003
+        int 0x15
+
+        shutdown_fail:
+        cli
+        hlt
+        jmp shutdown_fail
