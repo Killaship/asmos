bb6bd12f1e707ffbf7bee92bb8ba4517f201c795
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-11 17:43:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 git pull
 cd src/
-nasm -fbin -o ../os boot16.asm
+nasm -fbin -o ../bin/boot16 boot16.asm
+nasm -fbin -o ../bin/boot32 boot32.asm
 cd ..
+
 qemu-system-i386 -fda os -rtc base=localtime -curses
